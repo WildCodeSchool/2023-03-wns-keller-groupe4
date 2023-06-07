6036c152eb1229e0764db1097867b3d6d0cfc6d5
Yonahey <cherfi.ismael@gmail.comm>
2023-06-07 14:59:49 +0200

    feat/ Creating product and category entities

--- back/src/utils.ts
+++ back/src/utils.ts
@@ -1,15 +1,17 @@
-import { DataSource } from "typeorm";
-import { User } from "./entity/User";
+import {DataSource} from "typeorm";
+import {User} from "./entity/User";
+import {Product} from "./entity/Product";
+import {Category} from "./entity/Category";
 
 const dataSource = new DataSource({
-  type: "postgres",
-  host: "db",
-  port: 5432,
-  username: "postgres",
-  password: process.env.POSTGRES_PASSWORD,
-  database: "postgres",
-  synchronize: true,
-  entities: [User],
+	type: "postgres",
+	host: "db",
+	port: 5432,
+	username: "postgres",
+	password: process.env.POSTGRES_PASSWORD,
+	database: "postgres",
+	synchronize: true,
+	entities: [User, Product, Category],
 });
 
 export default dataSource;
