c17364cbc627288d5f2035ad7b15d3a5e9667f43
Cylecq <leclercq.cyril06@gmail.com>
2023-06-08 16:59:43 +0200

    add a search bar on home page

--- front/src/components/MenuFront.tsx
+++ front/src/components/MenuFront.tsx
@@ -1,13 +1,13 @@
 function MenuFront() {
   return (
     <div>
-      <label htmlFor="search" className="sr-only">
+      <label htmlFor="searchMenu" className="sr-only">
         Tous nos matériels
       </label>
       <input
         type="text"
-        name="search"
-        id="search"
+        name="searchMenu"
+        id="searchMenu"
         className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-main focus:border-transparent"
         placeholder="Chercher un matériel"
       />
