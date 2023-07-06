1c6b80fa8ae0db074eb10286363081f1e4cc074b
Val <titi-phenom-wcs@outlook.fr>
2023-07-06 13:19:25 +0200

    add comments

--- front/src/components/Front-Office/ProductsDetailsComponent.tsx
+++ front/src/components/Front-Office/ProductsDetailsComponent.tsx
@@ -35,51 +35,43 @@ const Product = ({ id, name, description, price, stock, picture, available }: IP
     return (
         <div className="min-h-screen">
             <div className="w-2/3 fill-height">
-            {/* <div className="grid grid-cols-2 gap-4 w-2/3 h-full"> */}
-                {/* <div className="col-span-2 text-3xl font-bold text-center">{name}</div> */}
+            
+                {/* Main image */}
                 <div className="flex justify-center">
                     <img className="md:w-48 lg:w-72" src={image} alt={name} />
                 </div>
-                {/* <div className="col-span-2 text-center">{description}</div>
-                <div className="text-left"><span className="font-bold">Status :</span>{ availability }</div>
-                <div className="text-right"><span className="font-bold">Price :</span> {price} €/day</div>
-                <div className="col-span-2 font-bold text-center">
-                    <button className="bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded inline-flex items-center" disabled={buttonState}>
-                    <span className="me-1">Add To Cart</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                        </svg>
-                    </button>
-                </div> */}
 
+                {/* Image gallery */}
                 <div className="grid gap-4 grid-cols-5 grid-rows-1 my-5">
-                <img
-                    src={image}
-                    alt={name}
-                    className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
-                />
-                <img
-                    src={image}
-                    alt={name}
-                    className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
-                />
-                <img
-                    src={image}
-                    alt={name}
-                    className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
-                />
-                <img
-                    src={image}
-                    alt={name}
-                    className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
-                />
-                <img
-                    src={image}
-                    alt={name}
-                    className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
-                />
+                    <img
+                        src={image}
+                        alt={name}
+                        className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
+                    />
+                    <img
+                        src={image}
+                        alt={name}
+                        className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
+                    />
+                    <img
+                        src={image}
+                        alt={name}
+                        className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
+                    />
+                    <img
+                        src={image}
+                        alt={name}
+                        className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
+                    />
+                    <img
+                        src={image}
+                        alt={name}
+                        className="sm:w-24 md:w-32 lg:w-32 border border-gray-300 hover:border-gray-500 rounded"
+                    />
                 </div>
             </div>
+
+            {/* Product details */}
             <div className="text-center absolute w-1/3 top-0 right-0 bottom-0 p-10 bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800"
                 data-te-sidenav-init data-te-sidenav-hidden="false" data-te-sidenav-position="absolute"
             >
