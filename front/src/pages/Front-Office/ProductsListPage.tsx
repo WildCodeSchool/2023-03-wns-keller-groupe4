f10f98815c98e196525559e6ed2f6618434fc73d
Val <titi-phenom-wcs@outlook.fr>
2023-07-10 10:29:11 +0200

    edit error message

--- front/src/pages/Front-Office/ProductsListPage.tsx
+++ front/src/pages/Front-Office/ProductsListPage.tsx
@@ -29,13 +29,18 @@ const ProductsList = () => {
   // Products data from API with pagination
   const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
+  if (loading) return <p className="text-center">Loading...</p>;
+  if (error) return (
+    <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+      <p className="font-bold">Erreur</p>
+      <p>Un problème est survenu.</p>
+    </div>
+  );
 
   return (
     <div className="my-10">
       <div className="mx-2 md:mx-5 lg:mx-10">
-        <h2 className="text-2xl text-center font-bold tracking-tight text-gray-900">All Products</h2>
+        <h2 className="text-2xl text-center font-bold tracking-tight text-gray-900">Tous nos produits</h2>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-3 lg:gap-5 mt-10 items-center place-items-stretch">
           {data.getProducts.map((product:any) => (
             <ProductsListComponent
