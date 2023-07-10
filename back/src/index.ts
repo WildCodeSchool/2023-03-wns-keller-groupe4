9dbbc51fc4a90148f8ccb2b7b7f1b0994f391a7a
Yonahey <cherfi.ismael@gmail.comm>
2023-07-10 10:44:21 +0200

    Feat/ Generating mock data

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,55 +2,166 @@ import "reflect-metadata";
 import * as dotenv from "dotenv";
 import * as jwt from "jsonwebtoken";
 import dataSource from "./utils";
-import { buildSchema } from "type-graphql";
-import { ApolloServer } from "apollo-server";
-import UserResolver from "./user/User.Resolver";
-import LangResolver from "./lang/Lang.Resolver";
+import {buildSchema} from "type-graphql";
+import {ApolloServer} from "apollo-server";
+// import {ProductService} from "./product/Product.Service";
+import UserResolver from "./user/UserResolver";
 import CategoryResolver from "./category/Category.Resolver";
 import ProductResolver from "./product/Product.Resolver";
-
+import {Category} from "./category/entity/Category";
+import {Product} from "./product/entity/Product";
+import {categoriesNames, mockProducts} from "./mockDataArray";
+import {ProductService} from "./product/Product.Service";
 
 dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET_KEY as string;
+export const DATA_FIXTURE_CATEGORIES = process.env
+	.DATA_FIXTURE_CATEGORIES as string;
+export const DATA_FIXTURE_PRODUCTS = process.env
+	.DATA_FIXTURE_PRODUCTS as string;
+
+const categoryRepository = dataSource.getRepository(Category);
+const productRepository = dataSource.getRepository(Product);
+const productService = new ProductService();
 
 if (JWT_SECRET === undefined) {
-    throw Error("JWT secret undefined");
+	throw Error("JWT secret undefined");
 }
 
 const start = async (): Promise<void> => {
-    await dataSource.initialize();
-    const typeGraphQLgeneratedSchema = await buildSchema({
-      validate: {forbidUnknownValues: false},
-      resolvers: [UserResolver, CategoryResolver, ProductResolver, LangResolver],
-      authChecker: ({context}) => {
-        if (context.email !== undefined) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-    });
+	await dataSource.initialize();
+	const typeGraphQLgeneratedSchema = await buildSchema({
+		validate: {forbidUnknownValues: false},
+		resolvers: [UserResolver, CategoryResolver, ProductResolver, LangResolver],
+		authChecker: ({context}) => {
+			if (context.email !== undefined) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+	});
 
-    const server = new ApolloServer({
-        schema: typeGraphQLgeneratedSchema,
-        context: ({ req }) => {
-            if (
-                req.headers.authorization !== undefined &&
-                req.headers.authorization !== ""
-            ) {
-                const payload = jwt.verify(
-                    req.headers.authorization.split("Bearer ")[1],
-                    JWT_SECRET
-                );
-                return payload;
-            }
-            return {};
-        },
-    });
+	const server = new ApolloServer({
+		schema: typeGraphQLgeneratedSchema,
+		context: ({req}) => {
+			if (
+				req.headers.authorization !== undefined &&
+				req.headers.authorization !== ""
+			) {
+				const payload = jwt.verify(
+					req.headers.authorization.split("Bearer ")[1],
+					JWT_SECRET
+				);
+				return payload;
+			}
+			return {};
+		},
+	});
 
-    const { url } = await server.listen();
-    console.log(`🚀  Server ready at ${url}`);
+	const {url} = await server.listen();
+
+	const dataFixture = async (): Promise<void> => {
+		if (DATA_FIXTURE_CATEGORIES === "true") {
+			// This creates the different categories, to add one simply modify the JSON in mockData.ts
+			for (let index = 0; index < categoriesNames.length; index++) {
+				await categoryRepository.save({
+					name: categoriesNames[index],
+				});
+			}
+			console.log("Categories Created");
+		}
+
+		// This is getting all the categories previously created so we can bind them to the products in next steps
+		const categories = await categoryRepository.find();
+
+		let resultArray = [];
+		// The issue is all the mock data for products doesn't have category provided. In the case where the category property of a product is a null array this aims to finds the matching category using the products name
+		if (DATA_FIXTURE_PRODUCTS === "true") {
+			console.log(`mockproduct array ${mockProducts.length}`);
+
+			mockProducts.forEach((product) => {
+				const CategoryNameFromProduct = product.name.slice(
+					0,
+					product.name.indexOf(" ")
+				);
+				// trying to find category using the product name
+
+				const foundMatchingCategory = categories.find((category) => {
+					return category.name === CategoryNameFromProduct;
+				});
+				console.log(foundMatchingCategory?.id);
+
+				// This checks if no category is provided for a product, if it's the case we check if we found it using the products name and finaly if the products doesn't already belong to the category
+				// console.log(product.categories.length === 0);
+
+				const needingToInferCategory =
+					product.categories.length === 0 &&
+					foundMatchingCategory !== undefined &&
+					!product.categories.includes(foundMatchingCategory.id);
+
+				// console.log(needingToInferCategory);
+
+				// If all that is true , we are going to add the infered category to our product
+				if (needingToInferCategory) {
+					product.categories = [
+						...product.categories,
+						foundMatchingCategory.id,
+					];
+				}
+
+				// we push the formated product in an array.
+
+				resultArray.push(product);
+			});
+
+			console.log(`result array ${resultArray.length}`);
+
+			// At this stage our products have categories so we can insert them in our database using a method from our productService
+			const allDbProducts = await productService.getAllProducts();
+			for (let index = 0; index < resultArray.length; index++) {
+				const productsToInsert = resultArray[index];
+				const {
+					name,
+					price,
+					stock,
+					available,
+					description,
+					picture,
+					categories,
+				} = productsToInsert;
+
+				const productAlreadyExists = allDbProducts.find((dbProduct) => {
+					return dbProduct.name === name;
+				});
+
+				if (productAlreadyExists === undefined) {
+					const newProduct = await productService.createNewProduct({
+						stock,
+						price,
+						name,
+						available,
+						description,
+						picture,
+						category: categories,
+					});
+					await productRepository.save(newProduct);
+				}
+			}
+			console.log("Products Created");
+		}
+	};
+
+	if (DATA_FIXTURE_CATEGORIES === "true" || DATA_FIXTURE_PRODUCTS === "true") {
+		void dataFixture();
+	} else {
+		console.log("data fixture off");
+	}
+	console.log(`🚀  Server ready at ${url}`);
+	// console.log(JWT_SECRET);
+	console.log(`DATA_FIXTURE_CATEGORIES ${DATA_FIXTURE_CATEGORIES}`);
+	// console.log(DATA_FIXTURE_PRODUCTS);
 };
 
 void start();
---
+++ back/src/mockDataArray.ts
@@ -0,0 +1,669 @@
+export const categoriesNames = [
+	"Bétonnière",
+	"Brouette",
+	"Camion",
+	"Compresseur",
+	"Dameuse",
+	"Décapeur",
+	"Décolleuse",
+	"Défonceuse",
+	"Diable",
+	"Echafaudage",
+	"Echelle",
+	"Escabeau",
+	"Groupe électrogène",
+	"Marteau piqueur",
+	"Mini pelle",
+	"Nacelle",
+	"Nettoyeur haute pression",
+	"Perforateur",
+	"Ponceuse",
+	"Scie",
+	"Tarière",
+	"Tondeuse",
+	"Tronçonneuse",
+	"Visseuse",
+];
+
+export interface Product {
+	name: string;
+	price: number;
+	stock: number;
+	available: boolean;
+	description: string;
+	picture: string;
+	categories: string[];
+}
+
+export const mockProducts: Product[] = [
+	{
+		name: "Bétonnière électrique 350L",
+		price: 30,
+		stock: 10,
+		available: true,
+		description: "Bétonnière électrique 350L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/0/100_0001_1.jpg",
+		categories: [],
+	},
+	{
+		name: "Bétonnière électrique 200L",
+		price: 20,
+		stock: 0,
+		available: false,
+		description: "Bétonnière électrique 200L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/0/100_0001_1.jpg",
+		categories: [],
+	},
+	{
+		name: "Bétonnière électrique 100L",
+		price: 10,
+		stock: 5,
+		available: true,
+		description: "Bétonnière électrique 100L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/0/100_0001_1.jpg",
+		categories: [],
+	},
+	{
+		name: "Brouette 100L",
+		price: 10,
+		stock: 12,
+		available: true,
+		description: "Brouette 100L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/0/100_0001_1.jpg",
+		categories: [],
+	},
+	{
+		name: "Brouette 200L",
+		price: 20,
+		stock: 11,
+		available: true,
+		description: "Brouette 200L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/0/100_0001_1.jpg",
+		categories: [],
+	},
+	{
+		name: "Dameuse 200kg",
+		price: 50,
+		stock: 3,
+		available: true,
+		description: "Dameuse 200kg",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/a/dameuse_100kg.jpg",
+		categories: [],
+	},
+	{
+		name: "Dameuse 300kg",
+		price: 60,
+		stock: 0,
+		available: false,
+		description: "Dameuse 300kg",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/a/dameuse_100kg.jpg",
+		categories: [],
+	},
+	{
+		name: "Compresseur 100L",
+		price: 22,
+		stock: 3,
+		available: true,
+		description: "Compresseur 100L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/o/compresseur_100l.jpg",
+		categories: [],
+	},
+	{
+		name: "Compresseur 200L",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Compresseur 200L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/o/compresseur_100l.jpg",
+		categories: [],
+	},
+	{
+		name: "Compresseur 300L",
+		price: 30,
+		stock: 1,
+		available: true,
+		description: "Compresseur 300L",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/o/compresseur_100l.jpg",
+		categories: [],
+	},
+	{
+		name: "Décapeur thermique 1000W",
+		price: 10,
+		stock: 0,
+		available: false,
+		description: "Décapeur thermique 1000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/decapeur_thermique_1000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Décapeur thermique 2000W",
+		price: 15,
+		stock: 3,
+		available: true,
+		description: "Décapeur thermique 2000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/decapeur_thermique_1000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Décolleuse papier peint 2000W",
+		price: 20,
+		stock: 0,
+		available: false,
+		description: "Décolleuse papier peint 2000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/decolleuse_papier_peint_2000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Décolleuse papier peint 3000W",
+		price: 25,
+		stock: 3,
+		available: true,
+		description: "Décolleuse papier peint 3000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/decolleuse_papier_peint_2000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Décolleuse papier peint 4000W",
+		price: 30,
+		stock: 0,
+		available: false,
+		description: "Décolleuse papier peint 4000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/decolleuse_papier_peint_2000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Défonçeuse 1000W",
+		price: 10,
+		stock: 3,
+		available: true,
+		description: "Défonçeuse 1000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/defonceuse_1000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Défonçeuse 2000W",
+		price: 15,
+		stock: 0,
+		available: false,
+		description: "Défonçeuse 2000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/defonceuse_1000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Défonceuse 3000W",
+		price: 20,
+		stock: 3,
+		available: true,
+		description: "Défonceuse 3000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/defonceuse_1000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Défonceuse 4000W",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Défonceuse 4000W",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/defonceuse_1000w.jpg",
+		categories: [],
+	},
+	{
+		name: "Diable à bavette",
+		price: 10,
+		stock: 3,
+		available: true,
+		description: "Diable à bavette",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/i/diable_a_bavette.jpg",
+		categories: [],
+	},
+	{
+		name: "Diable à bande",
+		price: 15,
+		stock: 0,
+		available: false,
+		description: "Diable à bande",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/i/diable_a_bavette.jpg",
+		categories: [],
+	},
+	{
+		name: "Diable chariot",
+		price: 20,
+		stock: 3,
+		available: true,
+		description: "Diable chariot",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/h/chariot_roulant.jpg",
+		categories: [],
+	},
+	{
+		name: "Echaffaudage 2m",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Echaffaudage 2m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/c/echafaudage_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Echaffaudage 4m",
+		price: 30,
+		stock: 3,
+		available: true,
+		description: "Echaffaudage 4m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/c/echafaudage_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Echaffaudage 6m",
+		price: 35,
+		stock: 0,
+		available: false,
+		description: "Echaffaudage 6m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/c/echafaudage_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Echelle 2m",
+		price: 10,
+		stock: 3,
+		available: true,
+		description: "Echelle 2m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/c/echelle_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Echelle 4m",
+		price: 15,
+		stock: 0,
+		available: false,
+		description: "Echelle 4m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/c/echelle_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Echelle 6m",
+		price: 20,
+		stock: 3,
+		available: true,
+		description: "Echelle 6m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/c/echelle_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Escabeau 2m",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Escabeau 2m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/s/escabeau_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Escabeau 4m",
+		price: 30,
+		stock: 3,
+		available: true,
+		description: "Escabeau 4m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/s/escabeau_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Escabeau 6m",
+		price: 35,
+		stock: 0,
+		available: false,
+		description: "Escabeau 6m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/e/s/escabeau_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Groupe électrogène 2kW",
+		price: 10,
+		stock: 3,
+		available: true,
+		description: "Groupe électrogène 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/r/groupe_electrogene_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Groupe électrogène 4kW",
+		price: 15,
+		stock: 0,
+		available: false,
+		description: "Groupe électrogène 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/r/groupe_electrogene_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Groupe électrogène 6kW",
+		price: 20,
+		stock: 3,
+		available: true,
+		description: "Groupe électrogène 6kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/r/groupe_electrogene_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Marteau piqueur 2kW",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Marteau piqueur",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/a/marteau_piqueur.jpg",
+		categories: [],
+	},
+	{
+		name: "Marteau piqueur 4kW",
+		price: 30,
+		stock: 3,
+		available: true,
+		description: "Marteau piqueur",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/a/marteau_piqueur.jpg",
+		categories: [],
+	},
+	{
+		name: "Marteau piqueur 6kW",
+		price: 35,
+		stock: 0,
+		available: false,
+		description: "Marteau piqueur",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/a/marteau_piqueur.jpg",
+		categories: [],
+	},
+	{
+		name: "Mini-pelle 2t",
+		price: 10,
+		stock: 3,
+		available: true,
+		description: "Mini-pelle 2t",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/i/mini-pelle_2t.jpg",
+		categories: [],
+	},
+	{
+		name: "Mini-pelle 4t",
+		price: 15,
+		stock: 0,
+		available: false,
+		description: "Mini-pelle 4t",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/i/mini-pelle_2t.jpg",
+		categories: [],
+	},
+	{
+		name: "Nacelle 2m",
+		price: 20,
+		stock: 3,
+		available: true,
+		description: "Nacelle 2m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/a/nacelle_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Nacelle 4m",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Nacelle 4m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/a/nacelle_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Nacelle 6m",
+		price: 30,
+		stock: 3,
+		available: true,
+		description: "Nacelle 6m",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/a/nacelle_2m.jpg",
+		categories: [],
+	},
+	{
+		name: "Nettoyeur haute pression 2kW",
+		price: 35,
+		stock: 0,
+		available: false,
+		description: "Nettoyeur haute pression 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/e/nettoyeur_haute_pression_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Nettoyeur haute pression 4kW",
+		price: 40,
+		stock: 3,
+		available: true,
+		description: "Nettoyeur haute pression 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/e/nettoyeur_haute_pression_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Nettoyeur haute pression 6kW",
+		price: 45,
+		stock: 0,
+		available: false,
+		description: "Nettoyeur haute pression 6kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/e/nettoyeur_haute_pression_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Perforateur 2kW",
+		price: 50,
+		stock: 3,
+		available: true,
+		description: "Perforateur 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/e/perforateur_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Perforateur 4kW",
+		price: 55,
+		stock: 0,
+		available: false,
+		description: "Perforateur 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/e/perforateur_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Perforateur 6kW",
+		price: 10,
+		stock: 3,
+		available: true,
+		description: "Perforateur 6kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/e/perforateur_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Ponceuse 2kW",
+		price: 15,
+		stock: 0,
+		available: false,
+		description: "Ponceuse 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/o/ponceuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Ponceuse 4kW",
+		price: 20,
+		stock: 3,
+		available: true,
+		description: "Ponceuse 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/o/ponceuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Scie à métaux",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Scie à métaux",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/c/scie_a_metaux.jpg",
+		categories: [],
+	},
+	{
+		name: "Scie à bois",
+		price: 30,
+		stock: 3,
+		available: true,
+		description: "Scie à bois",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/c/scie_a_metaux.jpg",
+		categories: [],
+	},
+	{
+		name: "Tarière 2kW",
+		price: 35,
+		stock: 0,
+		available: false,
+		description: "Tarière 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/a/tariere_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Tarière 4kW",
+		price: 40,
+		stock: 3,
+		available: true,
+		description: "Tarière 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/a/tariere_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Tarière 6kW",
+		price: 45,
+		stock: 0,
+		available: false,
+		description: "Tarière 6kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/a/tariere_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Tondeuse 2kW",
+		price: 50,
+		stock: 3,
+		available: true,
+		description: "Tondeuse 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/o/tondeuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Tondeuse 4kW",
+		price: 55,
+		stock: 0,
+		available: false,
+		description: "Tondeuse 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/o/tondeuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Tronçonneuse 2kW",
+		price: 10,
+		stock: 3,
+		available: true,
+		description: "Tronçonneuse 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/r/tronconneuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Tronçonneuse 4kW",
+		price: 15,
+		stock: 0,
+		available: false,
+		description: "Tronçonneuse 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/r/tronconneuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Visseuse 2kW",
+		price: 20,
+		stock: 3,
+		available: true,
+		description: "Visseuse 2kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/v/i/visseuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Visseuse 4kW",
+		price: 25,
+		stock: 0,
+		available: false,
+		description: "Visseuse 4kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/v/i/visseuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Visseuse 6kW",
+		price: 30,
+		stock: 3,
+		available: true,
+		description: "Visseuse 6kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/v/i/visseuse_2kw.jpg",
+		categories: [],
+	},
+	{
+		name: "Visseuse 8kW",
+		price: 35,
+		stock: 0,
+		available: false,
+		description: "Visseuse 8kW",
+		picture:
+			"https://www.loxam.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/v/i/visseuse_2kw.jpg",
+		categories: [],
+	},
+];
