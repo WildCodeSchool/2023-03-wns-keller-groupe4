47628118de8c6c27e9f7a50ec6eb7c7f5f6e255f
Cylecq <108573955+Cylecq@users.noreply.github.com>
2023-07-12 10:52:21 +0200

    Feat/stock-page (#20)
    * display stock
    
    * search bar done
    
    * css fix
    
    * rewrite getProducts in backend to add limit and offset + adapt the front
    
    * add pagination + delete sort data
    
    * handle sort data in backend
    
    * css fix
    
    * resolve comments mohammed
    
    * add category relation in getAllProduct

---
+++ back/src/product/inputs/GetProductsInput.ts
@@ -0,0 +1,19 @@
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class GetProductsInput {
+    @Field({ nullable: true })
+    limit?: number;
+
+    @Field({ nullable: true })
+    offset?: number;
+
+    @Field({ nullable: true })
+    name?: string;
+
+    @Field({ nullable: true, defaultValue: "name" })
+    orderBy: "name" | "price" | "stock" | "available" | "description" | "picture" | "created_at" | "updated_at" | "updated_by";
+
+    @Field({ nullable: true, defaultValue: "ASC" })
+    orderDirection: "ASC" | "DESC";
+}
