67dddcddcf0842f2be22a60021a3132a4beef9e4
Yonahey <cherfi.ismael@gmail.comm>
2023-07-12 12:04:53 +0200

    feat/Testing data fetching in the AdminProductDetail page

--- front/src/pages/Back-Office/AdminProductsDetails.tsx
+++ front/src/pages/Back-Office/AdminProductsDetails.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 import {useParams} from "react-router-dom";
+import {useQuery, gql} from "@apollo/client";
+import Products from "../../components/ProductsDetailsComponent";
+import {GET_ONE_PRODUCT} from "../Front-Office/ProductsDetailsPage";
 
 const AdminProductsDetails = () => {
 	const {productId} = useParams();
 	console.log(productId);
 
-	return <div>{productId}</div>;
+	const {loading, error, data} = useQuery(GET_ONE_PRODUCT, {
+		variables: {getProductId: productId},
+	});
+
+	// console.log(data);
+
+	const product = data.getProduct;
+	console.log(product);
+
+	console.log(typeof productId);
+	// if (loading) return <p>Loading...</p>;
+	// if (error) return <p>Error : {error.message}</p>;
+	return (
+		<div>
+			<main className="container mx-auto">
+				<section className="card-row mx-1">
+					<Products
+						key={product.id}
+						id={product.id}
+						name={product.name}
+						description={product.description}
+						stock={product.stock}
+						price={product.price}
+						picture={product.picture}
+						available={product.available}
+						isAdmin={true}
+					/>
+				</section>
+			</main>
+		</div>
+	);
 };
 
 export default AdminProductsDetails;
