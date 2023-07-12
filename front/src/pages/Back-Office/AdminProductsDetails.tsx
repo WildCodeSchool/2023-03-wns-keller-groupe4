2dd5ab598bcd2c3db12f2b445ea241956dae5778
Yonahey <cherfi.ismael@gmail.comm>
2023-07-12 11:24:00 +0200

    feat/ AddingProductDetail page

--- front/src/pages/Back-Office/AdminProductsDetails.tsx
+++ front/src/pages/Back-Office/AdminProductsDetails.tsx
@@ -2,32 +2,36 @@ import React from "react";
 import {useParams} from "react-router-dom";
 import {useQuery, gql} from "@apollo/client";
 import Products from "../../components/ProductsDetailsComponent";
-import {GET_ONE_PRODUCT} from "../Front-Office/ProductsDetailsPage";
+import {GET_ONE_PRODUCT} from "../../utils/queries";
 
 const AdminProductsDetails = () => {
   const {productId} = useParams();
   console.log(productId);
 
   const {loading, error, data} = useQuery(GET_ONE_PRODUCT, {
-    variables: {getProductId: productId},
+    variables: {getProductId: productId as string},
   });
+  const product = data?.getProduct;
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   return (
     <div>
       <main className="container mx-auto">
         <section className="card-row mx-1">
-          <Products
-            key={data.getProduct.id}
-            id={data.getProduct.id}
-            name={data.getProduct.name}
-            description={data.getProduct.description}
-            stock={data.getProduct.stock}
-            price={data.getProduct.price}
-            picture={data.getProduct.picture}
-            available={data.getProduct.available}
-            isAdmin={true}
-          />
+          {product && (
+            <Products
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              description={product.description}
+              stock={product.stock}
+              price={product.price}
+              picture={product.picture}
+              available={product.available}
+              isAdmin={false}
+            />
+          )}
         </section>
       </main>
     </div>
