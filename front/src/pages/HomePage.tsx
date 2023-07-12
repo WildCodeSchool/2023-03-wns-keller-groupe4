dfa3c9117da1e753203b984a1496a44a0519468b
Cylecq <leclercq.cyril06@gmail.com>
2023-07-12 10:21:46 +0200

    fix css

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -42,7 +42,7 @@ function HomePage() {
 
   return (
     <div className="relative flex flex-col items-center justify-around w-4/5 mx-auto h-[calc(100vh-58px)] lg:h-[calc(100vh-74px)]">
-      <div className="absolute top-8 w-4/5 sm:w-96 lg:top-24">
+      <div className="absolute top-8 w-4/5 sm:w-96 lg:top-24 sm-height:top-8">
         <Combobox value={selected} onChange={setSelected}>
           <div className="relative mt-1">
             <Combobox.Label className="opacity-50">
@@ -94,7 +94,7 @@ function HomePage() {
           </div>
         </Combobox>
       </div>
-      <div className="flex flex-col items-center border border-main rounded-xl shadow-sm py-8 mb-36 sm:w-80">
+      <div className="flex flex-col items-center border border-main rounded-xl shadow-sm py-8 mb-36 sm:w-80 sm-height:mb-0">
         <h1 className="text-4xl font-bold mb-4">Wildrent</h1>
         <p className="text-xl text-center">
           Louez votre matériel de chantier sans difficulté
