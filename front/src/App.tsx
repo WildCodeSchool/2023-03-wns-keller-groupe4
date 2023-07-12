22a58e9ebc33cd3cd2d0d34d2a35ad350ef9b3dd
Yonahey <cherfi.ismael@gmail.comm>
2023-07-12 11:24:30 +0200

    Adding route for AdminProductDetail

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 import Layout from "./components/Layout";
 
@@ -7,30 +7,77 @@ import HomePage from "./pages/Front-Office/HomePage";
 import ProductsListPage from "./pages/Front-Office/ProductsListPage";
 import ConnectFront from "./pages/Front-Office/ConnectFront";
 import CreateProduct from "./pages/Back-Office/CreateProduct";
-import Stock from "./pages/Back-Office/Stock";
 import Profile from "./pages/Front-Office/Profile";
+import AdminProductsDetails from "./pages/Back-Office/AdminProductsDetails";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout isFrontOffice={true} />}>
-        <Route index element={<HomePage />} />
-        <Route path="/connect" element={<ConnectFront />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/cart" element={<h1>Cart</h1>} />
-        <Route path="*" element={<h1>404</h1>} />
-        <Route path="/products/list/:categorySlug" element={<ProductsListPage />} />
-        <Route path="/product/:productId" element={<ProductDetails />} />
+      <Route
+        path="/"
+        element={<Layout isFrontOffice={true} />}
+      >
+        <Route
+          index
+          element={<HomePage />}
+        />
+        <Route
+          path="/connect"
+          element={<ConnectFront />}
+        />
+        <Route
+          path="/profile"
+          element={<Profile />}
+        />
+        <Route
+          path="/cart"
+          element={<h1>Cart</h1>}
+        />
+        <Route
+          path="*"
+          element={<h1>404</h1>}
+        />
+        <Route
+          path="/products/list/:categorySlug"
+          element={<ProductsListPage />}
+        />
+        <Route
+          path="/product/:productId"
+          element={<ProductDetails />}
+        />
       </Route>
-      <Route path="/admin" element={<Layout isFrontOffice={false} />}>
-        <Route path="/admin/create" element={<CreateProduct />} />
-        <Route path="/admin/stock" element={<Stock />} />
-        <Route path="/admin/stock/:id" element={<h1>Stock Product</h1>} />
-        <Route path="/admin/reservations" element={<h1>Reservations</h1>} />
-        <Route path="/admin/profile" element={<h1>Admin Profile</h1>} />
+      <Route
+        path="/admin"
+        element={<Layout isFrontOffice={false} />}
+      >
+        <Route
+          path="/admin/create"
+          element={<CreateProduct />}
+        />
+        <Route
+          path="/admin/product/:productId"
+          element={<AdminProductsDetails />}
+        />
+
+        <Route
+          path="/admin/stock"
+          element={<h1>Stock</h1>}
+        />
+        <Route
+          path="/admin/stock/:id"
+          element={<h1>Stock Product</h1>}
+        />
+        <Route
+          path="/admin/reservations"
+          element={<h1>Reservations</h1>}
+        />
+        <Route
+          path="/admin/profile"
+          element={<h1>Admin Profile</h1>}
+        />
       </Route>
     </Routes>
   );
-};
+}
 
 export default App;
