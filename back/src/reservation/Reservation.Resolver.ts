245bd547e649bcdd34aca1de3bd1189be3a0d695
Eli-Sensei <goldengames630@gmail.com>
2023-07-12 09:48:54 +0200

    ajour du dossier reservation

---
+++ back/src/reservation/Reservation.Resolver.ts
@@ -0,0 +1,66 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "./entity/Reservation";
+import UserService from "./Reservation.Service";
+import CreateUserInput from "./inputs/CreateReservationInput";
+import UpdateUserInput from "./inputs/UpdateReservationInput";
+import SignupUserInput from "./inputs/SignupUserInput";
+
+@Resolver()
+export default class UserResolver {
+    service;
+    constructor() {
+        this.service = new UserService();
+    }
+
+
+    @Query(() => User)
+    async login(
+        @Arg("email") email: string,
+        @Arg("password") password: string
+    ): Promise<User> {
+        return await this.service.login(email, password);
+    }
+
+    @Mutation(() => User)
+    async signup(
+        @Arg("signupUserInput") signupUserInput: SignupUserInput
+    ): Promise<User> {
+        return await this.service.signup(signupUserInput);
+    }
+
+    @Mutation(() => User)
+    async createUser(
+        @Arg("createUserInput") createUserInput: CreateUserInput
+    ): Promise<User> {
+        return await this.service.createOneUser(createUserInput);
+    }
+
+    // pour activer l'autorisation par token
+    // @Authorized()
+    @Query(() => [User])
+    async getUsers(): Promise<User[]> {
+        return await this.service.getAllUsers();
+    }
+
+    // pour activer l'autorisation par token
+    // @Authorized()
+    @Query(() => User)
+    async getUserById(@Arg("id") id: string): Promise<User> {
+        return await this.service.getOneUserById(id);
+    }
+
+    @Mutation(() => Boolean)
+    async updateUser(
+        @Arg("id") id: string,
+        @Arg("updateUserInput") updateUserInput: UpdateUserInput
+    ): Promise<Boolean> {
+        return await this.service.updateOneUser(id, updateUserInput);
+    }
+
+    @Mutation(() => Boolean)
+    async deleteUserById(
+        @Arg("id") id: string
+    ): Promise<Boolean> {
+        return await this.service.deleteOneUserById(id);
+    }
+}
---
+++ back/src/reservation/entity/Reservation.ts
@@ -0,0 +1,58 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    OneToOne,
+    PrimaryGeneratedColumn,
+} from "typeorm";
+import { Field, ObjectType, registerEnumType } from "type-graphql";
+import { UserProfile } from "./UserProfile";
+
+export enum EnumRoles {
+    SUPERADMIN = "superAdmin",
+    ADMIN = "admin",
+    USER = "user",
+}
+
+registerEnumType(EnumRoles, {
+    name: "EnumRoles",
+    description: "Liste des roles possible pour un utilisateurrrr",
+});
+
+@ObjectType()
+@Entity()
+export class User {
+    @Field()
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Field()
+    @Column({ unique: true })
+    email: string;
+
+    @Field()
+    @Column()
+    hashedPassword: string;
+
+    @Field()
+    @Column()
+    role: EnumRoles;
+
+    @Field()
+    @OneToOne((type) => UserProfile, { 
+        cascade: true, onDelete: 'CASCADE' 
+    })
+    @JoinColumn()
+    user_profile: UserProfile;
+
+    @Field()
+    @Column({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+    created_at: Date;
+
+    @Field()
+    @Column({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+    updated_at: Date;
+
+    @Field({nullable: true, description: "readonly"})
+    token?: string;
+}
