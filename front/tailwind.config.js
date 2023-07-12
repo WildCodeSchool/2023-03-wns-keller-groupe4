f8fd40a981875886c0cc8a0d75b9fad93a373728
Cylecq <leclercq.cyril06@gmail.com>
2023-07-12 12:24:57 +0200

    fix tailwind media queries

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
       },
     },
     screens: {
+      "sm": { raw: "(min-width: 640px)" },
+      "md": { raw: "(min-width: 768px)" },
+      "lg": { raw: "(min-width: 1024px)" },
+      "xl": { raw: "(min-width: 1280px)" },
+      "2xl": { raw: "(min-width: 1536px)" },
       "sm-height": { raw: "(max-height: 770px), (min-width: 1024px)" },
     }
   },
