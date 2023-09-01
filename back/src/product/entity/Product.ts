06dcc13d7acfefd5f31fcc90e872fa08339b5178
Yonahey <cherfi.ismael@gmail.comm>
2023-09-01 11:32:00 +0200

    feat(back-end): making it impssible to input negative values for product stock and prices

--- back/src/product/entity/Product.ts
+++ back/src/product/entity/Product.ts
@@ -1,58 +1,58 @@
 import {
-	Column,
-	Entity,
-	JoinTable,
-	ManyToMany,
-	PrimaryGeneratedColumn,
+    Column,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryGeneratedColumn,
 } from "typeorm";
-import {Field, ObjectType} from "type-graphql";
-import {Category} from "../../category/entity/Category";
+import { Field, ObjectType } from "type-graphql";
+import { Category } from "../../category/entity/Category";
 
 @ObjectType()
 @Entity()
 export class Product {
-	@Field()
-	@PrimaryGeneratedColumn("uuid")
-	id: string;
+    @Field()
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
 
-	@Field()
-	@Column({unique: true})
-	name: string;
+    @Field()
+    @Column({ unique: true })
+    name: string;
 
-	@Field()
-	@Column("decimal", {precision: 10, scale: 2})
-	price: number;
+    @Field()
+    @Column("decimal", { precision: 10, scale: 2 })
+    price: number;
 
-	@Field()
-	@Column()
-	stock: number;
+    @Field()
+    @Column()
+    stock: number;
 
-	@Field()
-	@Column()
-	available: boolean;
+    @Field()
+    @Column()
+    available: boolean;
 
-	@Field()
-	@Column("text")
-	description: string;
+    @Field()
+    @Column("text")
+    description: string;
 
-	@Field()
-	@Column("text")
-	picture: string;
+    @Field()
+    @Column("text")
+    picture: string;
 
-	@Field({nullable: true})
-	@Column({type: "timestamptz", default: () => "CURRENT_TIMESTAMP"})
-	created_at: Date;
+    @Field({ nullable: true })
+    @Column({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+    created_at: Date;
 
-	@Field()
-	@Column({type: "timestamptz", nullable: true})
-	updated_at: Date;
+    @Field()
+    @Column({ type: "timestamptz", nullable: true })
+    updated_at: Date;
 
-	@Field()
-	@Column({nullable: true})
-	updated_by: string;
+    @Field()
+    @Column({ nullable: true })
+    updated_by: string;
 
-	@Field(() => [Category])
-	@ManyToMany(() => Category, (category) => category.products)
-	@JoinTable()
-	categories: Category[];
+    @Field(() => [Category])
+    @ManyToMany(() => Category, (category) => category.products)
+    @JoinTable()
+    categories: Category[];
 }
--- back/src/product/inputs/CreateProductInput.ts
+++ back/src/product/inputs/CreateProductInput.ts
@@ -1,27 +1,30 @@
-import {Field, InputType} from "type-graphql";
-import {Product} from "../entity/Product";
+import { Field, InputType } from "type-graphql";
+import { Product } from "../entity/Product";
+import { Min } from "class-validator";
 
 @InputType()
 export class CreateProductInput implements Partial<Product> {
-	@Field()
-	name: string;
+    @Field()
+    name: string;
 
-	@Field()
-	price: number;
+    @Field()
+    @Min(0)
+    price: number;
 
-	@Field()
-	stock: number;
+    @Field()
+    @Min(0)
+    stock: number;
 
-	@Field()
-	available: boolean;
+    @Field()
+    available: boolean;
 
-	@Field()
-	description: string;
+    @Field()
+    description: string;
 
-	@Field()
-	picture: string;
+    @Field()
+    picture: string;
 
-	// Since a product can have many categories we expect an array of category ID here.
-	@Field((type) => [String])
-	category?: string[];
+    // Since a product can have many categories we expect an array of category ID here.
+    @Field((type) => [String])
+    category?: string[];
 }
