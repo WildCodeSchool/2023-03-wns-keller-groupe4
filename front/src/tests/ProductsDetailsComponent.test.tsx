7ce3761635ab5cd18325dc58274bfe5394e093fc
Val <titi-phenom-wcs@outlook.fr>
2023-09-01 13:59:57 +0200

    add more tests

--- front/src/tests/ProductsDetailsComponent.test.tsx
+++ front/src/tests/ProductsDetailsComponent.test.tsx
@@ -1,5 +1,7 @@
 import { act, fireEvent, render, screen, waitFor } from "../utils/testCustomRender";
 import ProductsDetailsComponent from "../components/ProductsDetailsComponent";
+import verifyBase64Image from "../utils/verifyBase64Image";
+import defaultImage from "./../assets/products/default.png";
 
 class ResizeObserver {
     observe() {}
@@ -14,20 +16,25 @@ describe("Products details", () => {
         render(<ProductsDetailsComponent id={"1"} name={"brouette"} description={""} price={10} stock={5} picture={"test"} available={true} />);
     });
 
-    it("renders elements of the component for an available product", () => {
-        render(<ProductsDetailsComponent id={"1"} name={"brouette"} description={"Description de la brouette"} price={10} stock={5} picture={"test"} available={true} />);
+    it("renders elements of the component for an available product", async () => {
+        render(<ProductsDetailsComponent id={"1"} name={"brouette"} description={"Description de la brouette"} price={10} stock={5} picture={"image.jpg"} available={true} />);
         expect(screen.getByRole("heading", { name: "brouette" })).toBeInTheDocument();
         expect(screen.getByText(/Description de la brouette/i)).toBeInTheDocument();
         expect(screen.getByText(/10 €/i)).toBeInTheDocument();
         expect(screen.getByText(/5 pieces/i)).toBeInTheDocument();
         expect(screen.getByText(/Reservation/i)).toBeInTheDocument();
+        expect(screen.getByRole("img")).toHaveAttribute("src", "image.jpg");
     });
 
-    describe("When the product is not available and there has not a description", () => {
+    describe("When the product is not available or have no description, or image src is wrong", () => {
         it("should display the status as unavailable", () => {
-            render(<ProductsDetailsComponent id={"1"} name={"brouette"} description={""} price={10} stock={5} picture={"test"} available={false} />);
-            expect(screen.getByText(/No description available/i)).toBeInTheDocument();
+            render(<ProductsDetailsComponent id={"1"} name={"brouette"} description={""} price={10} stock={5} picture={"image.jpg"} available={false} />);
             expect(screen.getByText(/Unavailable/i)).toBeInTheDocument();
+        });
+
+        it("should display the description as unavailable", () => {
+            render(<ProductsDetailsComponent id={"1"} name={"brouette"} description={""} price={10} stock={5} picture={"image.jpg"} available={false} />);
+            expect(screen.getByText(/No description available/i)).toBeInTheDocument();
         });
 
         it("should disable the reservation button", () => {
