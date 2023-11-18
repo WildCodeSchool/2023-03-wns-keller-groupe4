aae09e718585ebbb66138c5410af926eab1a8cd3
Ismaël <icherfi@apizr.fr>
2023-11-18 14:41:22 +0100

    feat(backend pagination):adding pagination to the search reservation function

--- back/src/reservation/inputs/SearchReservationInput.ts
+++ back/src/reservation/inputs/SearchReservationInput.ts
@@ -1,11 +1,5 @@
 import { InputType, Field } from "type-graphql";
-import {
-    IsString,
-    IsEmail,
-    IsOptional,
-    IsEnum,
-    IsObject,
-} from "class-validator";
+import { IsString, IsOptional, IsEnum, IsObject } from "class-validator";
 import { EnumStatusReservation } from "../entity/Reservation";
 
 @InputType()
@@ -30,6 +24,27 @@ export class SearchReservationInput {
     @IsOptional()
     @IsEnum(EnumStatusReservation)
     status?: EnumStatusReservation;
+
+    @Field({ nullable: true })
+    limit?: number;
+
+    @Field({ nullable: true })
+    offset?: number;
+
+    @Field({ nullable: true, defaultValue: "id" })
+    orderBy:
+        | "id"
+        | "price"
+        | "stock"
+        | "available"
+        | "description"
+        | "picture"
+        | "created_at"
+        | "updated_at"
+        | "updated_by";
+
+    @Field({ nullable: true, defaultValue: "ASC" })
+    orderDirection: "ASC" | "DESC";
 }
 
 @InputType()
