4cee2bbd7d320ed36c68c6b5301864ba457fc0cf
Cylecq <leclercq.cyril06@gmail.com>
2023-12-06 14:57:24 +0100

    Replace home by index to prevent crash

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -24,7 +24,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="home"
+        name="index"
         options={{
           title: "Accueil",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
