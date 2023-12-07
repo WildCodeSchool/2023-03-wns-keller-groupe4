a36d1c7469d52aedbb96842345d51ded95fcad94
Cylecq <leclercq.cyril06@gmail.com>
2023-12-07 11:19:47 +0100

    Add product details and styling to category page

--- mobile/app/categories/[category].tsx
+++ mobile/app/categories/[category].tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useQuery } from "@apollo/client";
+import { AntDesign } from "@expo/vector-icons";
 
 import { GET_PRODUCTS_BY_CATEGORY_NAME } from "../../constants/queries";
 import Colors from "../../constants/Colors";
@@ -47,7 +48,22 @@ const ProductListByCategory = ({}: Props) => {
   return (
     <View style={styles.container}>
       {products.map((product) => (
-        <Text key={product.id}>{product.name}</Text>
+        <View key={product.id} style={styles.product}>
+          <View style={styles.productLeft}>
+            <Image
+              source={{ uri: product.picture }}
+              style={styles.productImage}
+            />
+          </View>
+          <View style={styles.productRight}>
+            <Text style={styles.productName}>{product.name}</Text>
+            <Text style={styles.productPrice}>{product.price}€/jour</Text>
+            <Pressable style={styles.productDetailsButton}>
+              <Text style={styles.productDetailsButtonText}>Details</Text>
+              <AntDesign name="pluscircleo" size={18} color="white" />
+            </Pressable>
+          </View>
+        </View>
       ))}
     </View>
   );
@@ -60,5 +76,56 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     backgroundColor: Colors.light.background,
+  },
+  product: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "90%",
+    borderWidth: 1,
+    borderColor: Colors.light.orange,
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  productLeft: {
+    width: "50%",
+    height: 150,
+  },
+  productRight: {
+    width: "50%",
+    height: 150,
+    borderLeftWidth: 1,
+    borderLeftColor: Colors.light.lightGray,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  productImage: {
+    height: "100%",
+    width: "100%",
+    objectFit: "contain",
+  },
+  productName: {
+    fontFamily: "Rubik",
+    fontSize: 18,
+  },
+  productPrice: {
+    fontFamily: "RubikBold",
+    fontSize: 18,
+    color: Colors.light.orange,
+  },
+  productDetailsButton: {
+    backgroundColor: Colors.light.gray,
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+  productDetailsButtonText: {
+    fontFamily: "Rubik",
+    fontSize: 16,
+    color: "#fff",
   },
 });
