a0f7a7296f8e13b4cd3fb70b3fe5f712d0d4e900
Ismaël <icherfi@apizr.fr>
2024-01-06 14:25:12 +0100

    feat(homeSearchBar): CategorySearchBar done

--- front/src/utils/utils.ts
+++ front/src/utils/utils.ts
@@ -1,16 +1,1 @@
-// export const util = () => console.log("bonjour je susi utile");
-import { useEffect, useState } from "react";
-
-export function useDebounce(value: string, delay: number) {
-    const [debouncedValue, setDebouncedValue] = useState(value);
-
-    useEffect(() => {
-        const timerId = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delay);
-
-        return () => clearTimeout(timerId);
-    }, [value, delay]);
-
-    return debouncedValue;
-}
+export const util = () => console.log("bonjour je susi utile");
